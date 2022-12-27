b5adf18084f1243c8191edbae6daf4faba30eacb
Ryota Ohhashi <ryota.ohhashi@accenture.com>
2022-12-27 14:46:09 +0900

    modified

--- docs/index.js
+++ docs/index.js
@@ -17,8 +17,6 @@ var scene = new ScrollMagic.Scene({
   .addTo(controller);
 
 var kv = $(".kv").children();
-var len = kv.length;
-var perDuration = duration / len;
 var scroll = 0;
 // var easing = 0.1;
 // var smoothScroll = 0;
@@ -43,18 +41,22 @@ document.addEventListener('scroll', function () {
 });
 
 function switchImage(scroll) {
+
     // kv外の時はラストの画像のopacityを1にして早々にreturn
     if (scroll > duration) {
-      $(kv[len - 1]).css("opacity", 1);
+      $(kv[numImage- 1]).css("opacity", 1);
       return;
     }
 
     // 画像切り替え
     for (let i = 0; i < kv.length; i++) {
 
-      if ( perDuration * (i) < scroll && scroll <= perDuration * (i + 1)) {
-        const value = calcValue(scroll, i);
-        $(kv[i]).css("opacity", value);
+      if ( oneImageDuration * (i) < scroll && scroll <= oneImageDuration * (i + 1) ) {
+        const valueDec = calcValue(scroll, i, "dec");
+        const valueInc = calcValue(scroll, i, "inc");
+        if (i !== 0) $(kv[i - 1]).css("opacity", valueDec);
+        $(kv[i]).css("opacity", valueInc);
+
       }else{
         // スクロールが早い場合を考慮して、対象以外の画像のopacityを0に
         $(kv[i]).css("opacity", 0);
@@ -64,23 +66,18 @@ function switchImage(scroll) {
 }
 
 
-function calcValue(scroll, i) {
-  var diff = scroll - perDuration * i;
-  var normalized = diff / perDuration;
-  var value;
+function calcValue(scroll, i, direction) {
 
-  // 最初の画像以外は0→1→0
-  if (i !== len - 1) {
-    if(0 <= normalized && normalized < 0.5) value = normalized * 2;
-    if(0.5 <= normalized && normalized <= 1) value = (1 - normalized) * 2;
-    if(value < 0.05) value = 0;
-    return value;
+  const diff = scroll - oneImageDuration * i;
+  const normalized = diff / oneImageDuration;
 
-  // 最後の画像は0→1
-  }else{
-    value = normalized;
-    if(value > 0.95) value = 1;
-    return value;
+  // 増加方向
+  if (direction === "inc") {
+    return normalized
+
+  // 減少方向
+  }else if (direction === "dec"){
+    return 1 - normalized;
   }
 }
 
